b7fe9985f653a6fd6546c6aaeecf2f5e07e3a4d3
Richard-tenorio <rtenorio.k11937393@umak.edu.ph>
2025-05-13 22:41:40 +0800

    Update update.php - tenorio

--- activity/update.php
+++ activity/update.php
@@ -1,1 +1,20 @@
 
+<?php
+$conn = new mysqli("localhost", "root", "", "usercrud");
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $stmt = $conn->prepare("UPDATE users SET name=?, email=?, password=? WHERE id=?");
+    $stmt->bind_param("sssi", $name, $email, $password, $id);
+
+    if ($stmt->execute()) {
+        echo "User Updated Successfully";
+    } else {
+        echo "Error: " . $conn->error;
+    }
+}
+?>
