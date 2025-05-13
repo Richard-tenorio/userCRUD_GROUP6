99f0da88224182a6010b4371ed4eff2dd5028a6d
Richard-tenorio <rtenorio.k11937393@umak.edu.ph>
2025-05-13 22:39:46 +0800

    Update delete.php - tenorio

--- activity/delete.php
+++ activity/delete.php
@@ -1,1 +1,17 @@
 
+<?php
+$conn = new mysqli("localhost", "root", "", "usercrud");
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $id = $_POST['id'];
+
+    $stmt = $conn->prepare("DELETE FROM users WHERE id=?");
+    $stmt->bind_param("i", $id);
+
+    if ($stmt->execute()) {
+        echo "User Deleted Successfully";
+    } else {
+        echo "Error: " . $conn->error;
+    }
+}
+?>
