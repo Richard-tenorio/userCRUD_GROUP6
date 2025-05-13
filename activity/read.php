73b44e42a21379647e12c96f8e8e6aa47ce5adf7
Richard-tenorio <rtenorio.k11937393@umak.edu.ph>
2025-05-13 22:40:12 +0800

    Update read.php

--- activity/read.php
+++ activity/read.php
@@ -1,1 +1,12 @@
+<?php
+$conn = new mysqli("localhost", "root", "", "usercrud");
 
+$result = $conn->query("SELECT * FROM users");
+
+$users = array();
+while ($row = $result->fetch_assoc()) {
+    $users[] = $row;
+}
+
+echo json_encode($users);
+?>
